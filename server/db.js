3668e88212a7e92ade5f293be2a43ad4a969fc4e
Devesh Kundu <devesh.kundu28@gmail.com>
2024-03-07 07:42:19 +0530

    Made Schema and it's model for Instructors

--- server/db.js
+++ server/db.js
@@ -7,3 +7,25 @@ mongoose
   .then( () => console.log("Connected to MongoDB!") )
   .catch( () => console.error.bind(console, "MongoDB connection error:") );
 
+// Schema for Instructors
+const InstructorSchema = new mongoose.Schema({
+    name : {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+\@.+\..+/, "Please fill a valid email address"]
+    },
+    courses: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Course'
+    }]
+});
+
+// model for Instructors
+const InstructorCollection = mongoose.model('Instructor', InstructorSchema);
