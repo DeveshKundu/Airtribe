fce765f520eb8799a2822161367e21ec23ad0706
Devesh Kundu <devesh.kundu28@gmail.com>
2024-03-07 21:47:43 +0530

    created the retrieve and creation of course APIs

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { CourseCollection } = require('./db');
+const { CourseCollection, LearnerCollection, InstructorCollection, CommentCollection } = require('./db');
 const app = express();
 const port = 5000;
 
@@ -10,6 +10,25 @@ app.listen(port, () => {
 });
 
 app.get("/*", (req, res) => {
-    res.send("You got the route error");
+    res.send(`You got the route error`);
+});
+
+app.get("/courses", async (req, res) => {
+    try {
+        const courses = await CourseCollection.find({});
+        res.json({ courses });
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
+
+app.post("/courses", async (req, res) => {
+    const course = new CourseCollection(req.body);
+    try {
+        await course.save();
+        res.status(201).send(course);
+    } catch (error) {
+        res.status(400).send(error);
+    }
 });
 
