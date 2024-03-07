a41c8d6c10248d4ad4978c93bfbe8f826b85e0cc
Devesh Kundu <devesh.kundu28@gmail.com>
2024-03-07 18:45:24 +0530

    created database Airtribe in uri string

--- server/db.js
+++ server/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const uri = "mongodb+srv://deveshk28:6DsgooiaNWy3Z5iC@clustur.xvadqep.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://deveshk28:6DsgooiaNWy3Z5iC@clustur.xvadqep.mongodb.net/Airtribe?retryWrites=true&w=majority";
 
 mongoose
   .connect(uri)
