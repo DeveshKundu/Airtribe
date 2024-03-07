519a02e564b91b2360a83ddd4720fc259902dd52
Devesh Kundu <devesh.kundu28@gmail.com>
2024-03-07 07:53:49 +0530

    Made Schema and it's model for Learners

--- server/db.js
+++ server/db.js
@@ -27,5 +27,43 @@ const InstructorSchema = new mongoose.Schema({
     }]
 });
 
-// model for Instructors
+// Schema for Learners
+const LearnerSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+\@.+\..+/, "Please fill a valid email address"]
+    },
+    phone_number: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    linkedin_profile: {
+        type: String,
+        required: true
+    },
+    course: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Course'
+    },
+    status: {
+        type: String,
+        enum: ['Accepted', 'Rejected', 'Waitlist'],
+        default: 'Waitlist'
+    },
+    comments: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Comment'
+    }]
+});
+
+// All required models
 const InstructorCollection = mongoose.model('Instructor', InstructorSchema);
+const LearnerCollection = mongoose.model('Learner', LearnerSchema);
