cb7f27c486ba41e0fe5360f152cd2000882bec76
Devesh Kundu <devesh.kundu28@gmail.com>
2024-03-09 06:51:05 +0530

    created 2 docker files to run APIs

--- server/database/conn.js
+++ server/database/conn.js
@@ -2,10 +2,10 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   user: "postgres",
-  host: "localhost",
+  host: "db",
   database: "airtribe",
   password: "devesh",
-  port: 5000
+  port: 5432
 });
 
 module.exports = pool;
